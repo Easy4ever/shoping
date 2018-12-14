fd6dda1ab29458fd11a6d6d19c918300f02bfb0f
Saeed_Ahmed_Buraiah <easy4ever@outlook.com>
2018-12-14 19:13:04 +0300

    Add Best Sales

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
+
+    $('[data-add-to-cart]').click(function(e){
+        alert('أضيف المُنتج إلى عربة الشراء');
+        e.stopPropagation();
+    });
 });
