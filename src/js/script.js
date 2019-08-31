6f36e87e2e151bbc71b4ad32ea04d5c4a5f42e06
Saeed_Ahmed_Buraiah <easy4ever@outlook.com>
2019-08-31 04:41:49 +0300

    Complete checkout.html File

--- src/js/script.js
+++ src/js/script.js
@@ -10,4 +10,63 @@ $(document).ready(function(){
         $(this).parents('.product-option').siblings().removeClass('active');
         $(this).parents('.product-option').addClass('active');
     });
+
+    //حذف منتج من السلة
+
+    $('[data-remove-from-cart]').click(function(){
+        // تحديد العنصر المختار وحذفه
+        $(this).parents('[data-product-info]').remove();
+
+        // تحديث إجمالي المنتجات
+        calculateTotalPrice();
+    });
+
+    // عند تغيير كمية المنتج
+    $('[data-product-quantity]').change(function(){
+
+        // جلب القيمة الجديدة
+        var newQuantity = $(this).val();
+
+        //ابحث عن السطر الذي يحتوي معلومات هذا المنتج
+        var $parent =$(this).parents('[data-product-info]');
+
+        //جلب سعر القطعة الواحدة من معلومات المنتج
+        var pricePerUnit = $parent.attr('data-product-price');
+
+        //عملية حسابية لمعرفة إجمالي سعر المنتج
+        var totalPriceForProduct = newQuantity * pricePerUnit;
+
+        //عين السعر الجديد ضمن خلية السعر الإجمالي للمنتج في هذا السطر
+        $parent.find('.total-price-for-product').text(totalPriceForProduct + '$');
+
+        // نداء الدالة جمع كل المنتجات
+        calculateTotalPrice();
+    });
+
+    // أمر حساب إجمالي جميع المنتجات
+    
+    function calculateTotalPrice(){
+
+        //إنشئ متغير لحفظ السعر الإجمالي ونعطيه قيمة ابتدائي صفر
+        var totalPriceForAllProducts = 0;
+
+        //نقوم بعمل أمر حلقة دورانية ليقوم بالمرور على جميع المنتجات وتطبيق عليهم الاوامر التالي
+        $('[data-product-info]').each(function(){
+            
+            // جلب سعر القطعة الواحدة
+            var pricePerUnit = $(this).attr('data-product-price');
+
+            //جلب كمية المنتج
+            var quantity = $(this).find('[data-product-quantity]').val();
+
+            //عملية ضرب بين السعر وعدد
+            var totalPriceForProduct = pricePerUnit * quantity;
+
+            // إضافة قيمة إجمالي المنتج في قيمة إجمالي جميع المنتجات
+            totalPriceForAllProducts = totalPriceForAllProducts + (totalPriceForProduct);
+        });
+
+        $('#total-price-for-all-products').text(totalPriceForAllProducts);
+    }
+
 });
