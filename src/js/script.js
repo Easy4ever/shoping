b23c7b4dbc66e23f232fbe353eb6e33c2a78dd5b
Saeed_Ahmed_Buraiah <easy4ever@outlook.com>
2019-08-31 21:57:55 +0300

    Complete payment.html File

--- src/js/script.js
+++ src/js/script.js
@@ -69,4 +69,60 @@ $(document).ready(function(){
         $('#total-price-for-all-products').text(totalPriceForAllProducts);
     }
 
+    $( "#submit-to-payment" ).click(function( event ) {
+        event.preventDefault(); // هام لمنع السلوك الافتراضي
+        
+        $(location).attr('href', "payment.html");
+      });
+
+    // إضافة المدن حسب الدولة
+    var citiesBycountry = {
+        ye: ['غيل باوزير','المكلا','عدن'],
+        sa: ['الرياض','جدة'],
+        eg: ['القاهرة','الإسكندرية'],
+        jo: ['عمان','الزرقاء'],
+        sy: ['دمشق','حلب','حماه']
+    };
+    // عندما يتغير الدولة يتم اختيار المدينة من المصفوفة الاعلى
+    $('#form-checkout select[name="country"]').change(function(){
+        //جلب رمز الدولة
+        var country = $(this).val();
+
+        //جلب مدن من المصفوفه الاعلى
+        var cities = citiesBycountry[country];
+
+        //تفريغ قائمة المدن القديمة
+        $('#form-checkout select[name="city"]').empty();
+        $('#form-checkout select[name="city"]').append(
+            '<option disabled selected value="">اختر المدينة</option>'
+        );
+
+        //أضف المدن إلى القائمة
+        cities.forEach(function(city){
+            var $newOption = $('<option></option>');
+            $newOption.text(city);
+            $newOption.val(city);
+
+            $('#form-checkout select[name="city"]').append($newOption);
+        });
+    });
+
+    // عند اختيار طريقة الدفع إلى بطاقة ائتمانية تفعل الحقول والعكس تقفل
+    $('#form-checkout input[name="payment_method"]').change(function(){
+
+        //جلب القيمة المختارة
+        var paymentMethod = $(this).val();
+
+        if(paymentMethod === 'on_delivery'){
+            //إذا كانت الاختيار على الدفع عند الاستلام يقوم إقفل الحقول
+            $('#credit-card-info input').prop('disabled',true);
+        }else{
+            // في حال العكس فعل الحقول
+            $('#credit-card-info input').prop('disabled',false);
+        }
+
+        //بدل معلومات بطاقة الائتمانية بين الظهور والاخفاء
+        $('#credit-card-info').toggle();
+    });
+
 });
